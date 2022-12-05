6997530c2fc60e87918ee251ce3c86b92de93900
becp12 <rebecca-preece@outlook.com>
2022-12-05 14:27:17 -0800

    Adding Header & NavBar

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import "./Header.css";
+import NavBar from "./NavBar";
+
+export default function Header() {
+    return (
+        <>
+            <div className="header">
+                <img src="https://i.imgur.com/JD0CjdK.jpg" />
+                <h1>App Name Placeholder</h1>
+                <NavBar />
+            </div>
+        </>
+    )
+}
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import "./NavBar.css";
+
+export default function NavBar() {
+    return (
+        <>
+            <div className="nav-bar">
+                <ul>
+                    <li>NEED</li>
+                    <li>DONATE</li>
+                    <li>LOGIN</li>
+                    <li>LOGOUT</li>
+                </ul>
+            </div>
+        </>
+    )
+}
