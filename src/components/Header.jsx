ccdff8f1ee841250d9bc479601f542141b001a30
becp12 <rebecca-preece@outlook.com>
2022-12-05 15:00:55 -0800

    Adjusting CSS to optomise mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,10 @@ export default function Header() {
     return (
         <>
             <div className="header">
-                <img src="https://i.imgur.com/JD0CjdK.jpg" />
-                <h1>App Name Placeholder</h1>
+                <div className="logo-title">
+                    <img src="https://i.imgur.com/JD0CjdK.jpg" />
+                    <h1 className="title">Title Placeholder</h1>
+                </div>
                 <NavBar />
             </div>
         </>
