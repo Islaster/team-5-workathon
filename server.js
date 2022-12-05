e06d9dad675837b75a48fd811ae89678673c91f9
badMeme <noveladdress@gmail.com>
2022-12-05 15:57:29 -0600

    testing db connection

--- server.js
+++ server.js
@@ -6,21 +6,26 @@ const app = express();
 require("dotenv").config();
 app.use(logger("dev"));
 app.use(express.json());
-// Configure both serve-favicon & static middleware
-// to serve from the production 'build' folder
-app.use(favicon(path.join(__dirname, "build", "favicon.ico")));
-app.use(express.static(path.join(__dirname, "build")));
 
-// The following "catch all" route (note the *) is necessary
-// to return the index.html on all non-AJAX requests
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+//-----
+    // Configure both serve-favicon & static middleware
+    // to serve from the production 'build' folder
+    app.use(favicon(path.join(__dirname, "build", "favicon.ico")));
+    app.use(express.static(path.join(__dirname, "build")));
 
-// Configure to use port 3001 instead of 3000 during
-// development to avoid collision with React's dev server
-const port = process.env.PORT || 3001;
+    // The following "catch all" route (note the *) is necessary
+    // to return the index.html on all non-AJAX requests
+    app.get("/*", function (req, res) {
+      res.sendFile(path.join(__dirname, "build", "index.html"));
+    });
 
-app.listen(port, function () {
-  console.log(`Express app running on port ${port}`);
-});
+    // Configure to use port 3001 instead of 3000 during
+    // development to avoid collision with React's dev server
+    const port = process.env.PORT || 3001;
+
+    app.listen(port, function () {
+      console.log(`Express app running on port ${port}`);
+    });
+//-----
+
+
