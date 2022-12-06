3e1e47840df82b3a2d18d0bfcb7cd39789df7ec1
becp12 <rebecca-preece@outlook.com>
2022-12-06 10:38:32 -0800

    Changing navbar location & words to icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,15 @@
 import "./Footer.css";
+import NavBar from "../components/NavBar"
 
 export default function Footer() {
     return (
         <>
             <div className="footer">
-                &copy; 2022 GA Winter Hackathon Bobcats. All Rights Reserved
+                <NavBar className="navbar" />
+                <div>
+                    &copy; 2022 GA Winter Hackathon Bobcats. All Rights Reserved<br/>
+                    <span className="tiny-text">Icon by <a href="https://freeicons.io/profile/3">icon king1</a> on <a href="https://freeicons.io">freeicons.io</a></span>
+                </div>
             </div>
         </>
     )
