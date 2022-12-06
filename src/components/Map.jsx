0ba99e100c31a98ce52a64479296d7d4bda07f4b
Brian-Patterson <pbpatterson92@gmail.com>
2022-12-06 11:18:20 -0800

    map on show page

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -9,15 +9,15 @@ import Point from "ol/geom/Point";
 import { circular } from "ol/geom/Polygon";
 import Control from "ol/control/Control";
 
-const Map = (props) => {
-// const userLat = document.getElementById("lat");
-// const userLong = document.getElementById("long");
-// const sendCoords = document.getElementById("sendCoords");
-// console.log(userLat);
-// console.log(userLong);
-// console.log(sendCoords);
+const MapImg = (props) => {
+const userLat = document.getElementById("lat");
+const userLong = document.getElementById("long");
+const sendCoords = document.getElementById("sendCoords");
+console.log(userLat);
+console.log(userLong);
+console.log(sendCoords);
 
-const mapImg = new Map({
+const map = new Map({
   target: "map-container",
   layers: [
     new TileLayer({
@@ -35,7 +35,7 @@ const layer = new VectorLayer({
   source: source,
 });
 
-mapImg.addLayer(layer);
+map.addLayer(layer);
   navigator.geolocation.watchPosition(
     function (pos) {
       const coords = [pos.coords.longitude, pos.coords.latitude];
@@ -43,7 +43,7 @@ mapImg.addLayer(layer);
       source.clear(true);
       source.addFeatures([
         new Feature(
-          accuracy.transform("EPSG:4326", mapImg.getView().getProjection())
+          accuracy.transform("EPSG:4326", map.getView().getProjection())
         ),
         new Feature(new Point(fromLonLat(coords))),
       ]);
@@ -57,18 +57,19 @@ mapImg.addLayer(layer);
     }
   );
 
+
 const locate = document.createElement("div");
 locate.className = "ol-control ol-unselectable locate";
 locate.innerHTML = '<button title="Locate me">◎</button>';
 locate.addEventListener("click", function () {
   if (!source.isEmpty()) {
-    mapImg.getView().fit(source.getExtent(), {
+    map.getView().fit(source.getExtent(), {
       maxZoom: 13,
       duration: 500,
     });
   }
 });
-mapImg.addControl(
+map.addControl(
   new Control({
     element: locate,
   })
@@ -81,4 +82,4 @@ mapImg.addControl(
     )
 }
 
-export default Map
+export default MapImg
--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,10 +1,10 @@
-import Map from "../components/Map"
+import MapImg from "../components/Map"
 
 const Show = (props) => {
     return (
         <>
         <h1>Show Page</h1>
-        <Map />
+        <MapImg />
         </>
     )
 }
