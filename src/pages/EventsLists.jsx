0d879c28f823475d1d76ebcd654a6b1691ea25ae
becp12 <rebecca-preece@outlook.com>
2022-12-07 15:44:23 -0800

    Fixing heart placement on Events List Page

--- src/pages/EventsLists.jsx
+++ src/pages/EventsLists.jsx
@@ -11,13 +11,14 @@ const EventsList = (props) => {
     <Card className="event-card">
       <Card.Body className="event-body">
         <blockquote className="blockquote mb-0">
-            <Card.Title className="card-title">Marin Food Bank Food Drive <span className="fav-icon"><img src="https://cdn-icons-png.flaticon.com/128/1000/1000621.png" style={{width: "2rem"}}/></span></Card.Title>
-          <Card.Text>
+          <Card.Title className="card-title">Marin Food Bank Food Drive </Card.Title>
+          <span className="fav-icon"><img src="https://cdn-icons-png.flaticon.com/128/1000/1000621.png" style={{width: "2rem"}}/></span>
+          <Card.Text className="row-2">
             12/9/2022 6:00 AM to 4:00PM 
             <br></br>
             <span className="details-card">http://marinfoodbank.org/events</span>
           </Card.Text>
-          <footer className="blockquote-footer">
+          <footer className="blockquote-footer row-3">
             <span><img src="https://cdn-icons-png.flaticon.com/128/484/484167.png" style={{width: "1.5rem"}}/></span> 1612 Lincoln St, CA San Francisco 94016
           </footer>
         </blockquote>
@@ -27,13 +28,14 @@ const EventsList = (props) => {
          <Card className="event-card">
       <Card.Body className="event-body">
         <blockquote className="blockquote mb-0">
-            <Card.Title>Mission Food Hub Meal Service <span className="fav-icon"><img src="https://cdn-icons-png.flaticon.com/128/1000/1000621.png" style={{width: "2rem"}}/></span></Card.Title>
-          <Card.Text>
+          <Card.Title>Mission Food Hub Meal Service </Card.Title>
+          <span className="fav-icon"><img src="https://cdn-icons-png.flaticon.com/128/1000/1000621.png" style={{width: "2rem"}}/></span>
+          <Card.Text className="row-2">
             12/9/2022 9:00 AM to 9:45 AM 
             <br></br>
             <span className="details-card">Mission Food Hub will be distrubuting Grab&Go meals provided by Second Harvest.</span>
           </Card.Text>
-          <footer className="blockquote-footer">
+          <footer className="blockquote-footer row-3">
           <span><img src="https://cdn-icons-png.flaticon.com/128/484/484167.png" style={{width: "1.5rem"}}/></span> 1923 Jackson St, CA San Francisco 94016
           </footer>
         </blockquote>
@@ -45,13 +47,14 @@ const EventsList = (props) => {
     <Card className = "event-card">
       <Card.Body className="event-body">
         <blockquote className="blockquote mb-0">
-            <Card.Title>Alta Plaza Park Meal Service: SE corner <span className="fav-icon"><img src="https://cdn-icons-png.flaticon.com/128/1000/1000621.png" style={{width: "2rem"}}/></span></Card.Title>
-          <Card.Text>
+          <Card.Title>Alta Plaza Park Meal Service: SE corner </Card.Title>
+          <span className="fav-icon"><img src="https://cdn-icons-png.flaticon.com/128/1000/1000621.png" style={{width: "2rem"}}/></span>
+          <Card.Text className="row-2">
             12/10/2022 6:00 AM to 4:00 PM
             <br></br>
             <span className="details-card">Alta Plaza Parks & Recreation will be distrubting Grab&Go meals provided by Feed America </span>
           </Card.Text>
-          <footer className="blockquote-footer">
+          <footer className="blockquote-footer row-3">
           <span><img src="https://cdn-icons-png.flaticon.com/128/484/484167.png" style={{width: "1.5rem"}}/></span> 3749 27th, CA San Francisco 94016
           </footer>
         </blockquote>
@@ -61,13 +64,14 @@ const EventsList = (props) => {
     <Card className = "event-card">
       <Card.Body className="event-body">
         <blockquote className="blockquote mb-0">
-            <Card.Title>Marin Food Bank Food Drive <span className="fav-icon"><img src="https://cdn-icons-png.flaticon.com/128/1000/1000621.png" style={{width: "2rem"}}/></span></Card.Title>
-          <Card.Text>
+          <Card.Title>Marin Food Bank Food Drive </Card.Title>
+          <span className="fav-icon"><img src="https://cdn-icons-png.flaticon.com/128/1000/1000621.png" style={{width: "2rem"}}/></span>
+          <Card.Text className="row-2">
             12/9/2022 6:00 AM to 4:00 PM
             <br></br>
             <span className="details-card">http://marinfoodbank.org/events</span>
           </Card.Text>
-          <footer className="blockquote-footer">
+          <footer className="blockquote-footer row-3">
           <span><img src="https://cdn-icons-png.flaticon.com/128/484/484167.png" style={{width: "1.5rem"}}/></span> 2039 35th, CA San Francisco 94016
           </footer>
         </blockquote>
