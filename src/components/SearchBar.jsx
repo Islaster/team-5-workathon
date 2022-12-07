620fa914c558229ad92bf5fa757d5a87de5e3c22
becp12 <rebecca-preece@outlook.com>
2022-12-06 16:14:00 -0800

    Adding search bar to home page

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,18 @@
+export default function SearchBar(props) {
+    return (
+        <>
+            <div className="search-container">
+                <form onSubmit="" className="search-form">
+                    <input
+                        type="text"
+                        name="search"
+                        onChange=""
+                        placeholder="Food Pantry, City or Zip..."
+                        value=""
+                    />
+                    <button type="submit">Search</button>
+                </form>
+            </div>
+        </>
+    )
+}
